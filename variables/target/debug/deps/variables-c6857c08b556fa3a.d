00ac4d980d0a109f495669084097c7787a20db23
Edan Barrios <dr.edandre@protonmail.com>
2025-06-17 22:54:37 -0300

    Variable Practice

---
+++ variables/target/debug/deps/variables-c6857c08b556fa3a.d
@@ -0,0 +1,5 @@
+/Users/edan/Desktop/VS_Code/rust_practice/variables/target/debug/deps/variables-c6857c08b556fa3a: src/main.rs
+
+/Users/edan/Desktop/VS_Code/rust_practice/variables/target/debug/deps/variables-c6857c08b556fa3a.d: src/main.rs
+
+src/main.rs:
---
+++ variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/Users/edan/Desktop/VS_Code/rust_practice/variables/target/debug/variables: /Users/edan/Desktop/VS_Code/rust_practice/variables/src/main.rs
